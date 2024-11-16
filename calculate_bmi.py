58f932ee8e0a0cf2e7308ee4c1e852a7253cd26d
MyatThetHmu <MYATTHET.23@ichat.sp.edu.sg>
2024-11-16 15:38:24 +0800

    Updated code

--- calculate_bmi.py
+++ calculate_bmi.py
@@ -7,11 +7,14 @@ def calculate_bmi(height, weight):
 
  if bmi < 18.5:
     print("Under Weight")
+    return -1
 
  elif bmi>= 18.5 and bmi <=25.0:
     print("Normal Weight")
-
+    return 0
  else:
     print("Over Weight")      
-
+    return 1
+ 
+ 
 calculate_bmi(weight=87, height=1.73)
